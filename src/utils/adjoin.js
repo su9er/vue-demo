c0cf7f2c12baacc42e74aa098906bfe286e3fe19
su9er.liang <su9er.liang@vipshop.com>
2020-07-01 10:38:27 +0800

    style: 修复格式问题

--- src/utils/adjoin.js
+++ src/utils/adjoin.js
@@ -1,59 +1,59 @@
 export default class Adjoin {
   constructor(vertex) {
-    this.vertex = vertex;
-    this.quantity = vertex.length;
-    this.init();
+    this.vertex = vertex
+    this.quantity = vertex.length
+    this.init()
   }
 
   init() {
     this.adjoinArray = Array.from({
       length: this.quantity * this.quantity
-    });
+    })
   }
 
   getVertexRow(id) {
-    const index = this.vertex.indexOf(id);
-    const col = [];
+    const index = this.vertex.indexOf(id)
+    const col = []
     this.vertex.forEach((item, pIndex) => {
-      col.push(this.adjoinArray[index + this.quantity * pIndex]);
-    });
-    return col;
+      col.push(this.adjoinArray[index + this.quantity * pIndex])
+    })
+    return col
   }
 
   getAdjoinVertexs(id) {
-    return this.getVertexRow(id).map((item, index) => (item ? this.vertex[index] : '')).filter(Boolean);
+    return this.getVertexRow(id).map((item, index) => (item ? this.vertex[index] : '')).filter(Boolean)
   }
 
   setAdjoinVertexs(id, sides) {
-    const pIndex = this.vertex.indexOf(id);
+    const pIndex = this.vertex.indexOf(id)
     sides.forEach((item) => {
-      const index = this.vertex.indexOf(item);
-      this.adjoinArray[pIndex * this.quantity + index] = 1;
-    });
+      const index = this.vertex.indexOf(item)
+      this.adjoinArray[pIndex * this.quantity + index] = 1
+    })
   }
 
   getRowTotal(params) {
-    params = params.map(id => this.getVertexRow(id));
-    const adjoinNames = [];
+    params = params.map(id => this.getVertexRow(id))
+    const adjoinNames = []
     this.vertex.forEach((item, index) => {
       const rowtotal = params.map(value => value[index]).reduce((total, current) => {
-        total += current || 0;
-        return total;
-      }, 0);
-      adjoinNames.push(rowtotal);
-    });
-    return adjoinNames;
+        total += current || 0
+        return total
+      }, 0)
+      adjoinNames.push(rowtotal)
+    })
+    return adjoinNames
   }
 
   // 交集
   getUnions(params) {
-    const row = this.getRowTotal(params);
-    return row.map((item, index) => item >= params.length && this.vertex[index]).filter(Boolean);
+    const row = this.getRowTotal(params)
+    return row.map((item, index) => item >= params.length && this.vertex[index]).filter(Boolean)
   }
 
   // 并集
   getCollection(params) {
-    params = this.getRowTotal(params);
-    return params.map((item, index) => item && this.vertex[index]).filter(Boolean);
+    params = this.getRowTotal(params)
+    return params.map((item, index) => item && this.vertex[index]).filter(Boolean)
   }
 }
